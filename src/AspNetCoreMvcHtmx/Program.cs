6d5fc4083c723212aa12a6c1c8ca8f667eb8c8c7
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2024-01-22 05:47:10 +0100

    refactor: shorten view references, rename controller to match vsa style

--- src/AspNetCoreMvcHtmx/Program.cs
+++ src/AspNetCoreMvcHtmx/Program.cs
@@ -37,7 +37,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
   name: "default",
-  pattern: "{controller=Home}/{action=Index}/{id?}"
+  pattern: "{controller=GetHome}/{action=GetHome}/{id?}"
 );
 
 app.Run();
